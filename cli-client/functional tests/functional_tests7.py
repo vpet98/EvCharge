acec8e5c1ff7bd236d44e9b3b7b8c6eed47fdab0
IliasTriant <hliastriant1@gmail.com>
2021-03-12 13:12:21 +0200

    fixed

--- cli-client/functional tests/functional_tests7.py
+++ cli-client/functional tests/functional_tests7.py
@@ -21,7 +21,18 @@ def test_login():
 	runner = CliRunner()
 	result = runner.invoke(ev_group46.Login, ['--username', 'admin', '--passw' ,'petrol4ever'])
 	assert result.exit_code == 0
+	#print(result.output)
 	assert result.output == 'login was successful\n'
+	
+	
+def sessions_upd():
+	runner = CliRunner()
+	token_file = open(path_of_token, 'r')
+	tok = json.load(token_file)
+	apikey = tok['token']
+	result = runner.invoke(ev_group46.Admin, ['--sessionsupd', '--source', 'demo.csv' ,'--apikey', apikey])
+	assert result.exit_code == 0
+	assert result.output == 'sessionsupd : done\n'
 	
 	
 def test_SessionsPerPoint():
@@ -29,40 +40,40 @@ def test_SessionsPerPoint():
 	token_file = open(path_of_token, 'r')
 	tok = json.load(token_file)
 	apikey = tok['token']
-	result = runner.invoke(ev_group46.SessionsPerPoint, ['--point', '5f69795300355e4c010658b6_119350', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
-	#print(result.output)
+	result = runner.invoke(ev_group46.SessionsPerPoint, ['--point', '5f6978bb00355e4c01059bc7_5096', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
+	print(result.output)
 	assert result.exit_code == 0
-	assert '0.747' in result.output
+	assert '0.746999979019165' in result.output
 	
 def test_SessionsPerStation():
 	runner = CliRunner()
 	token_file = open(path_of_token, 'r')
 	tok = json.load(token_file)
 	apikey = tok['token']
-	result = runner.invoke(ev_group46.SessionsPerStation, ['--station', '5f69795300355e4c010658b6', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
+	result = runner.invoke(ev_group46.SessionsPerStation, ['--station', '5f6978bb00355e4c01059bc7', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
 	#print(result.output)
 	assert result.exit_code == 0
-	assert '0.747' in result.output
+	assert '0.746999979019165' in result.output
 	
 def test_SessionsPerProvider():
 	runner = CliRunner()
 	token_file = open(path_of_token, 'r')
 	tok = json.load(token_file)
 	apikey = tok['token']
-	result = runner.invoke(ev_group46.SessionsPerProvider, ['--provider', '(Business Owner at Location)', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
+	result = runner.invoke(ev_group46.SessionsPerProvider, ['--provider', 'ESB Ecars', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
 	#print(result.output)
 	assert result.exit_code == 0
-	assert '0.747' in result.output
+	assert '0.746999979019165' in result.output
 	
 def test_SessionsPerEV():
 	runner = CliRunner()
 	token_file = open(path_of_token, 'r')
 	tok = json.load(token_file)
 	apikey = tok['token']
-	result = runner.invoke(ev_group46.SessionsPerEV, ['--ev', '926efa33-4c55-43df-80f3-4b8d2d65a00a', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
+	result = runner.invoke(ev_group46.SessionsPerEV, ['--ev', '45b68c71-cd11-4bd7-a03f-fdaae259635d', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', apikey])
 	print(result.output)
 	assert result.exit_code == 0
-	assert '0.747' in result.output
+	assert '0.746999979019165' in result.output
 
 def test_logout():
 	runner = CliRunner()
--- cli-client/functional tests/functional_tests8.py
+++ cli-client/functional tests/functional_tests8.py
@@ -33,6 +33,15 @@ def test_newuser1():
 	assert result.exit_code == 0
 	assert result.output == 'User registered successfully!\nusermod : done\n'
 
+def sessions_upd():
+	runner = CliRunner()
+	token_file = open(path_of_token, 'r')
+	tok = json.load(token_file)
+	apikey = tok['token']
+	result = runner.invoke(ev_group46.Admin, ['--sessionsupd', '--source', 'demo.csv' ,'--apikey', apikey])
+	assert result.exit_code == 0
+	assert result.output == 'sessionsupd : done\n'
+	
 def test_logout1():
 	runner = CliRunner()
 	token_file = open(path_of_token, 'r')
@@ -50,7 +59,7 @@ def test_login2():
 		
 def test_SessionsPerPoint1():
 	runner = CliRunner()
-	result = runner.invoke(ev_group46.SessionsPerPoint, ['--point', '5f69795300355e4c010658b6_119350', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYxNTMyMDQ0N30.mwPWMrQ5vlt14aoyoelvtRBUIZPZJbOGDVggV81FC4YxQWlms_6dziLYsf2Q1oPWIIargNIKl2r3k7yZCNbhbA'])
+	result = runner.invoke(ev_group46.SessionsPerPoint, ['--point', '5f6978bb00355e4c01059bc7_5096', '--datefrom', '20190901', '--dateto', '20190902', '--apikey', 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYxNTMyMDQ0N30.mwPWMrQ5vlt14aoyoelvtRBUIZPZJbOGDVggV81FC4YxQWlms_6dziLYsf2Q1oPWIIargNIKl2r3k7yZCNbhbA'])
 	#print(result.output)
 	assert result.exit_code == 0
 	assert result.output == 'you have to be a logged in user\n'
@@ -60,10 +69,10 @@ def test_SessionsPerEV():
 	token_file = open(path_of_token, 'r')
 	tok = json.load(token_file)
 	apikey = tok['token']
-	result = runner.invoke(ev_group46.SessionsPerEV, ['--ev', '926efa33-4c55-43df-80f3-4b8d2d65a00a', '--datefrom', '20190901', '--dateto', '20190902', '--format', 'csv', '--apikey', apikey])
+	result = runner.invoke(ev_group46.SessionsPerEV, ['--ev', '45b68c71-cd11-4bd7-a03f-fdaae259635d', '--datefrom', '20190901', '--dateto', '20190902', '--format', 'csv', '--apikey', apikey])
 	print(result.output)
 	assert result.exit_code == 0
-	assert '0.747' in result.output
+	assert '0.746999979019165' in result.output
 	
 def test_logout2():
 	runner = CliRunner()
