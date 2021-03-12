139944d4511ff088e0d7b9f94895b7320034c906
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2021-03-12 19:17:20 +0200

    fix unit test bug

--- cli-client/unit_tests/unit_tests1.py
+++ cli-client/unit_tests/unit_tests1.py
@@ -5,11 +5,10 @@ import ev_group46
 import urllib3
 urllib3.disable_warnings()
 
-def check_passw1():
+def test_check_passw1():
 	result = ev_group46.check_password("er rre")
 	assert result == False
-	
-def check_passw2():
+
+def test_check_passw2():
 	result = ev_group46.check_password("flflfflr12345##$!!==rg4+")
 	assert result == True
-
